5763f0b113fcc306d3f2876b1c90ac8bce011c59
Rahma SRIDI <104599931+RahmaSRIDI@users.noreply.github.com>
2022-10-29 12:19:45 +0200

    Ajout de la foctionnalités de routage

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus recusandae quae iste rerum. Provident laboriosam ex quibusdam voluptatem commodi, libero eligendi neque accusantium atque vitae unde aperiam quod amet blanditiis quae tempore incidunt hic dolor dicta maiores, adipisci laudantium fugit? Sapiente natus distinctio, eveniet impedit commodi quia rem dicta quisquam, nostrum, tempora eius ipsa at. Vel provident hic rerum magni pariatur. Consectetur dolores ipsam voluptas quasi ducimus, dicta explicabo excepturi eum odio sed omnis maxime, vitae possimus sapiente ipsa id similique. Consectetur accusantium eos quae autem non minus reiciendis. Quia facilis magni ab nam expedita. Quod esse odio reiciendis nostrum.</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor maiores at, soluta minima excepturi ipsam consequatur. Aliquid facere consectetur maiores, ab dolorem repellendus voluptates cupiditate, sapiente voluptas possimus incidunt? Commodi, nobis adipisci officia placeat corrupti, vero, nisi quam tenetur exercitationem ratione illum fugiat nam magni quod id recusandae dolorem ex. Id ab quibusdam dolorum omnis quos inventore nam, numquam vitae fuga, mollitia nulla tenetur sed reiciendis impedit assumenda aspernatur deleniti accusamus nobis perspiciatis provident molestiae quaerat! Ratione aperiam eos voluptas repellendus dolore quod illo nobis temporibus tempora voluptate est, saepe, error facilis, autem quos? Soluta, atque. Mollitia rem molestias nesciunt fugit modi minus in ipsa qui odio, reprehenderit quos reiciendis dicta hic et voluptatum alias beatae, accusamus officiis ex natus molestiae explicabo totam cum? Alias modi aliquid nam asperiores est repellendus quas officiis accusamus, ipsum aspernatur dignissimos quod obcaecati nulla quo sequi illo voluptatem distinctio tenetur omnis ab possimus odio nisi minima quos. Sit, quas pariatur. Vel officia et tempore accusamus aut. Beatae ut sint impedit eaque natus consequatur dolorem. Quae tenetur amet quibusdam voluptate et a veritatis cumque delectus. Tempore, dolores. Inventore beatae deserunt blanditiis quibusdam quas veniam distinctio esse. Id, quas dolor qui explicabo facilis beatae delectus voluptate.</p>
+        </div>
+    );
+};
+
+export default About;
