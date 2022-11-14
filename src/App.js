902365ef0b91c477170cf7ca0da0b47d8c55fb7d
Rahma SRIDI <104599931+RahmaSRIDI@users.noreply.github.com>
2022-11-14 08:53:23 +0100

    route page home corrigé

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
 
+        <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path="logement/:logementId" element={<Logement />} />
